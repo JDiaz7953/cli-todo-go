45686c13bb824732783eeb445a7a0715792c4237
Jaiden <113065377+JDiaz7953@users.noreply.github.com>
2026-04-10 15:17:11 -0400

    added list commands

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"github/jdiaz7953/cli-todo-go/printErrors"
 	"os"
 	"slices"
 	"strconv"
 	"strings"
 	"time"
-	"github/jdiaz7953/cli-todo-go/printErrors"
 )
 
 type Todo struct {
@@ -23,13 +23,22 @@ type Todo struct {
 func main(){
 	var allTask []Todo
 	idCounter := 1
+	session := true
+	scanner := bufio.NewScanner(os.Stdin)
+
+	//Get persistant data
+	data, err := os.ReadFile("listOfTask.json")
+	if err == nil {
+		json.Unmarshal(data, &allTask)
+		idCounter = len(allTask) + 1
+	}
+
 
 	//REPL for the cli
-	for true{
+	for session {
 
 		//Get the user input
 		fmt.Print("$todo-cli ")
-		scanner := bufio.NewScanner(os.Stdin)
 		scanner.Scan()
 		userInput := scanner.Text()
 		input := strings.Fields(userInput)
@@ -48,6 +57,10 @@ func main(){
 				printHelp()
 			}
 		case "add":
+
+			if len(input) == 1 {
+				fmt.Println(`you must add a valid argument (type "help" to see commands)`)
+			}
 
 			//Adds a task to the user json file 
 			textAfterCommand := strings.Join(input[1:], " ")
@@ -75,6 +88,12 @@ func main(){
 				idCounter++
 
 		case "update":
+
+			if len(input) == 1 {
+			   fmt.Println(`you must add a valid argument (type "help" to see commands)`)
+			   break
+			}
+
 			taskId, _:= strconv.Atoi(input[1])
 			textAfterCommand := strings.Join(input[2:], " ")
 
@@ -99,25 +118,101 @@ func main(){
 			}
 
 		case "delete":
+			if len(input) == 1 {
+			   fmt.Println(`you must add a valid argument (type "help" to see commands)`)
+			   break
+			}
 
 			//Delete task based on ID
 			taskId, _ := strconv.Atoi(input[1])
 			for i := 0; i < len(allTask); i++ {
 				if allTask[i].Id == taskId {
-					allTask = slices.Delete(allTask, i, 1)
+					allTask = slices.Delete(allTask, i, i+1)
 				}
 			}
 
 		case "mark-in-progress":
+			if len(input) >= 3 {
+				fmt.Println("")
+				fmt.Println(input[2], "is not a valid argument")
+				fmt.Println("")
+				break
+			}else if len(input) == 1 {
+			   fmt.Println(`you must add a valid argument (type "help" to see commands)`)
+			   break
+			}
+			//Mark a task based on the ID as in-progress
+			taskId, _ := strconv.Atoi(input[1])
+			for i := 0; i < len(allTask); i++ {
+				if allTask[i].Id == taskId {
+					allTask[i].Status = "in-progress"
+				}
+			}
+		case "mark-done":
+			if len(input) >= 3 {
+				fmt.Println("")
+				fmt.Println(input[2], "is not a valid argument")
+				fmt.Println("")
+				break
+			}else if len(input) == 1 {
+			   fmt.Println(`you must add a valid argument (type "help" to see commands)`)
+			   break
+			}
+			taskId, _ := strconv.Atoi(input[1])
+			for i := 0; i < len(allTask); i++ {
+				if allTask[i].Id == taskId {
+					allTask[i].Status = "done"
+				}
+			}
+
+		case "list":
+			if len(input) >= 3 {
+				fmt.Println("")
+				fmt.Println(input[2], "is not a valid argument")
+				fmt.Println("")
+				break
+			}else if len(input) == 1 {
+			   fmt.Println(`you must add a valid argument (type "help" to see commands)`)
+			   break
+			}
+
+			if input[1] == "done" {
+				for i := 0; i < len(allTask); i++ {
+					if allTask[i].Status == "done" {
+						fmt.Println(allTask[i].Description)
+						fmt.Println(" ")
+					}
+				}
+			}
+			if input[1] == "todo" {
+				for i := 0; i < len(allTask); i++ {
+					if allTask[i].Status == "todo" {
+						fmt.Println(allTask[i].Description)
+						fmt.Println(" ")
+					}
+				}
+			}
+			if input[1] == "in-progress" {
+				for i := 0; i < len(allTask); i++ {
+					if allTask[i].Status == "in-progress" {
+						fmt.Println(allTask[i].Description)
+						fmt.Println(" ")
+					}
+				}
+			}
+		case "save-exit":
+			session = false
+
+		default:
+			fmt.Println(" ")
+			fmt.Println(`please enter a valid command (type "help" to see commands)`)
 		}
-
-		
-		//Processes JSON Data
-		allTaskJson, _ := json.Marshal(allTask)
-
-		os.WriteFile("listOfTask.json", allTaskJson,0644)
 
 	}
 	//Display a help section to understand commands
+
+		//Processes JSON Data
+		allTaskJson, _ := json.Marshal(allTask)
+		os.WriteFile("listOfTask.json", allTaskJson,0644)
 
 }
